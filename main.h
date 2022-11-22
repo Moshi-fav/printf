7c1fe833b5db0cd20f22b8d84b17b5e7c84c47a9
Moshi <101973043+Moshi-fav@users.noreply.github.com>
2022-11-22 19:15:26 +0800

    main.h

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+
+#endif
